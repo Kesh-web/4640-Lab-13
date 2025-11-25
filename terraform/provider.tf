a8dbad5e47ae30d9900c12cc5bb5fabe326c6935
alfred-debian <alfred-debian@alfred.wifi.bcit.ca>
2025-11-25 15:02:31 -0800

    added files

---
+++ terraform/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket        = "acit4640-lab13bucket"
+    key           = "terraform.tfstate"
+    region        = "us-west-2"
+    encrypt       = true
+    use_lockfile  = true
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
